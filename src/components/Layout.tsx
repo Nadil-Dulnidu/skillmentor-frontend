d7d897eac020d2f86f7795125a7d854bee0b2f4c
Nadil Dulnidu <nzinlud963@gmail.com>
2025-07-19 22:42:06 +0530

    chore: add sonner insted of toast ui

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,17 +1,17 @@
 import { Navigation } from "./Navigation";
 import { Footer } from "./Footer";
 import { ReactNode } from "react";
-import { ToastProvider, ToastViewport } from "./ui/toast";
+import { Toaster } from "sonner";
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
-    <ToastProvider>
+      <>
       <section className="min-h-screen flex flex-col">
         <Navigation />
-        <main>{children}</main>
+        <main className="flex-1">{children}</main>
         <Footer />
       </section>
-      <ToastViewport />
-    </ToastProvider>
+      <Toaster/>
+      </>
   );
 }
